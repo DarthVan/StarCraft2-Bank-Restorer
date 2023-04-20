888517992426a3b659b18e960c6a10e957ff05c5
Star Noob <van.kol@mail.ru>
2023-04-20 11:05:30 +0300

    version 1.04

--- src/Globals.ts
+++ src/Globals.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION: string = '1.04';
+export const APP_VERSION: string = '1.04 (20.04.2023)';
 export const CHECK_UPDATES_DELAY: number = 3600000; // 1 hour
 export const TOTAL_BG_PICTURES: number = 4;
 export const CHANGE_BG_DELAY: number = 120000; // 2min
